ff8f434b8f126d6efa82ee0bea43702ce50518cc
R. Quazi <112246222+qriz1452@users.noreply.github.com>
2024-05-26 20:32:24 +0530

    Update ansible_server_conf.sh

--- prod/ansible_server_conf.sh
+++ prod/ansible_server_conf.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Function to display ASCII art for ANSIBLE
-
-
 display_banner() {
   echo " ///////////////////////////////////"
   echo " ///                             ///"
@@ -11,48 +9,33 @@ display_banner() {
   echo " ///  CONFIGURATION  SCRIPT      ///"
   echo " ///                             ///"
   echo " /////////////////////////////////// "
-
   echo " "
   echo "The Script started at  : $(date +"%Y-%m-%d %H:%M:%S")"
-
 }
 
-
 # Function to check system details
-
 sys_info() {
-
   echo "Fetching OS Details......"
-  # For most modern Linux Distributions
   if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
-  # For older versions of ubuntu
   elif type lsb_release >/dev/null 2>&1; then
     OS=$(lsb_release -si)
-  # For older version of ubuntu without lsb_release command
   elif [ -f /etc/lsb_release ]; then
     . /etc/lsb_release
     OS=$DISTRIB_ID
-  # For Debian
   elif [ -f /etc/debian_version ]; then
     OS=Debian
-  # For RHEL - CentOS, Fedora, Amazon Linux
   elif [ -f /etc/redhat-release ]; then
     OS=$(cat /etc/redhat-release | awk '{print $1}')
-  # Fallback to uname
   else
     OS=$(uname -s0)
   fi
-
   echo "Detected OS  is  : $OS"
 }
 
-
-
-
 # Function to check python and install if not available.
-py_check(){
+py_check() {
   echo "Checking Python version..."
   if command -v python3 &>/dev/null; then
     PYTHON_VERSION=$(python3 --version)
@@ -64,9 +47,8 @@ py_check(){
         sudo apt update -y
         sudo apt install python3 -y
         ;;
-
       "CentOS" | "Red Hat Enterprise Linux" | "Fedora" | "Amazon Linux")
-        sudo yum  update -y
+        sudo yum update -y
         sudo yum install python3 -y
         ;;
       *)
@@ -79,16 +61,13 @@ py_check(){
       echo "Failed to install Python."
     fi
   fi
-
 }
 
-
 # Ansible User creation
-ansible_user(){
-
-ANSIBLE_USER="ansible"
-ANSIBLE_GROUP="ansible"
-ANSIBLE_DIR="/home/ansible"
+ansible_user() {
+  ANSIBLE_USER="ansible"
+  ANSIBLE_GROUP="ansible"
+  ANSIBLE_DIR="/home/ansible"
 
   if grep -q "^$ANSIBLE_GROUP:" /etc/group; then
     echo "Ansible group --> $ANSIBLE_GROUP <-- already exist."
@@ -99,7 +78,6 @@ ANSIBLE_DIR="/home/ansible"
     echo "Group Created."
   fi
 
-
   if id "$ANSIBLE_USER" &>/dev/null; then
     echo "Ansible user --> $ANSIBLE_USER <-- already exist."
   else
@@ -108,7 +86,6 @@ ANSIBLE_DIR="/home/ansible"
     sudo useradd -m -d $ANSIBLE_DIR -g $ANSIBLE_GROUP -s /bin/bash $ANSIBLE_USER
     echo "User created"
   fi
-
 
   if [ -d "$ANSIBLE_DIR" ]; then
     echo "Ansible directory --> $ANSIBLE_DIR <-- already exist"
@@ -119,46 +96,43 @@ ANSIBLE_DIR="/home/ansible"
     echo "Ansible Home directory created."
   fi
 
-
-
-
   sudo chown -R $ANSIBLE_USER:$ANSIBLE_GROUP $ANSIBLE_DIR
   echo "Ansible home directory permissions updated"
   sudo chmod 755 $ANSIBLE_DIR
   echo "Ansible chmod done to 755"
 
-  echo "Ansible user,group and directory configurations Completed."
-
-  echo "Reviewing and setting permission for ansible home directory , ssh and authorized keys"
-  chmod 0700 $ANSIBLE_DIR
-  chmod 0700 $SSH_DIR
-  touch $SSH_DIR/authorized_keys
-  chmod 0600 $SSH_DIR/authorized_keys
-  
+  echo "Ansible user, group, and directory configurations Completed."
 }
 
-
 ansible_sec() {
-
-ANSIBLE_USER_PASS="password"
-SSH_DIR="$ANSIBLE_DIR/.ssh"
-KEY_NAME="ansible_server_id_rsa"
+  ANSIBLE_USER_PASS="password"
+  SSH_DIR="/home/ansible/.ssh"
+  KEY_NAME="ansible_server_id_rsa"
 
   echo "Setting ansible user password"
-  #echo "$ANSIBLE_USER_PASS" | sudo passwd --stdin $ANSIBLE_USER
   echo "${ANSIBLE_USER}:${ANSIBLE_USER_PASS}" | sudo chpasswd
-
 
   if [ ! -d $SSH_DIR ]; then
     echo "$SSH_DIR does not exist, creating..."
-    mkdir -p "$SSH_DIR"
+    sudo mkdir -p "$SSH_DIR"
     echo "$SSH_DIR created."
     echo "Setting $SSH_DIR permission..."
-    chown "$ANSIBLE_USER:$ANSIBLE_USER" "$SSH_DIR"
-    chmod 700 "$SSH_DIR"
+    sudo chown "$ANSIBLE_USER:$ANSIBLE_USER" "$SSH_DIR"
+    sudo chmod 700 "$SSH_DIR"
+    echo "$SSH_DIR directory permissions updated."
+  else
+    echo "Setting $SSH_DIR permission..."
+    sudo chown "$ANSIBLE_USER:$ANSIBLE_USER" "$SSH_DIR"
+    sudo chmod 700 "$SSH_DIR"
     echo "$SSH_DIR directory permissions updated."
   fi
 
+  echo "Reviewing and setting permission for ansible home directory, ssh, and authorized keys"
+  sudo chmod 0700 $ANSIBLE_DIR
+  sudo chmod 0700 $SSH_DIR
+  sudo touch $SSH_DIR/authorized_keys
+  sudo chmod 0600 $SSH_DIR/authorized_keys
+  sudo chown "$ANSIBLE_USER:$ANSIBLE_USER" $SSH_DIR/authorized_keys
 
   # Path to the sshd_config file
   SSHD_CONFIG="/etc/ssh/sshd_config"
@@ -167,7 +141,6 @@ KEY_NAME="ansible_server_id_rsa"
   update_sshd_config() {
       local setting="$1"
       local value="$2"
-
       if grep -q "^#*$setting" "$SSHD_CONFIG"; then
           sudo sed -i "s|^#*$setting.*|$setting $value|" "$SSHD_CONFIG"
       else
@@ -177,22 +150,18 @@ KEY_NAME="ansible_server_id_rsa"
 
   # Ensure PubkeyAuthentication is set to yes
   update_sshd_config "PubkeyAuthentication" "yes"
-
-  # Ensure PasswordAuthentication is set to no
+  # Ensure PasswordAuthentication is set to yes
   update_sshd_config "PasswordAuthentication" "yes"
-
   # Ensure AuthorizedKeysFile is set to .ssh/authorized_keys
   update_sshd_config "AuthorizedKeysFile" ".ssh/authorized_keys"
-
   update_sshd_config "PermitRootLogin" "no"
   update_sshd_config "ChallengeResponseAuthentication" "no"
   update_sshd_config "UsePAM" "yes"
-  update_sshd_config "# #GSSAPIAuthentication" "yes"
-  update_sshd_config "# #GSSAPICleanupCredentials" "no"
+  update_sshd_config "GSSAPIAuthentication" "yes"
+  update_sshd_config "GSSAPICleanupCredentials" "no"
 
   # Restart SSH service to apply changes
   sudo systemctl restart sshd
-
   echo "SSH configuration updated and SSH service restarted."
 
   # Add ansible to sudoers without password prompt
@@ -202,13 +171,7 @@ KEY_NAME="ansible_server_id_rsa"
   else
       echo "ansible user already has NOPASSWD sudo privileges."
   fi
-
-
-
 }
-
-
-
 
 # Display the banner
 display_banner
@@ -219,10 +182,8 @@ sys_info
 # Installing Python
 py_check
 
-
 # Ansible user creation
 ansible_user
 
-
-#Ansible ssh and passwd
+# Ansible ssh and passwd
 ansible_sec
